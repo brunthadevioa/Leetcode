9c500a2b8c26be56413e4d563098798de6447839
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-07-05 16:28:30 +0530

    Time: 2 ms (99.26%) | Memory: 44.4 MB (65.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
+
         int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+
+        int end = s.length()-1;
+
+        while(start<=end){
+
+            char first = s.charAt(start);
+
+            char last = s.charAt(end);
+
+            if(!Character.isLetterOrDigit(first)){
+
+                start++;
+            }
+
+            else if(!Character.isLetterOrDigit(last)){
+
+                end--;
+            }
+
+            else{
+
+                if(Character.toLowerCase(first)!=Character.toLowerCase(last)){
+
+                    return false;
+                }
+
+                start++;
+
+                end--;
+            }
         }
+
         return true;
+        
     }
 }
