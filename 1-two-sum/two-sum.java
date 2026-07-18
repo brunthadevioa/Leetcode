a683a6e279ab26a97006c9fa95e5d4e98fd66eba
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-07-18 21:45:47 +0530

    Time: 2 ms (99.30%) | Memory: 47.1 MB (58.61%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,23 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int n = nums.length;
-//two sum
+        int ans[] = new int[2];
+
         HashMap<Integer,Integer> map = new HashMap<>();
+
+        int n = nums.length;
 
         for(int i=0;i<n;i++){
 
-            int r = target - nums[i];
+            int rem = target - nums[i];
 
-            if(map.containsKey(r)){
+            if(map.containsKey(rem)){
 
-                return new int[]{map.get(r),i};
+                return new int[]{map.get(rem),i};
             }
 
             map.put(nums[i],i);
         }
 
         return new int[]{};
+
+
+
         
     }
 }
