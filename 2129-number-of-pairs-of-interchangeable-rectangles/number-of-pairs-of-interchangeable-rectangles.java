f1fb2dbe91b0434256fec4d9042027873ef9f704
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-07-18 22:38:54 +0530

    Time: 45 ms (50.61%) | Memory: 149.4 MB (48.16%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
@@ -1,15 +1,15 @@
 class Solution {
     public long interchangeableRectangles(int[][] rectangles) {
 
-        int n = rectangles.length;
-
         long count = 0;
 
         HashMap<Double,Integer> map = new HashMap<>();
 
+        int n = rectangles.length;
+
         for(int i=0;i<n;i++){
 
-            double ratio = rectangles[i][0]/(double)rectangles[i][1];
+            double ratio = rectangles[i][0]/ (double) rectangles[i][1];
 
             if(map.containsKey(ratio))
 
@@ -17,6 +17,7 @@ class Solution {
 
             map.put(ratio,map.getOrDefault(ratio,0)+1);
         }
+
         return count;
         
     }
