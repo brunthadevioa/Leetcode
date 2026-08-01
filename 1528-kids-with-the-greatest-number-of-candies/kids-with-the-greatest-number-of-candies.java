e90d4f7de3988ba5cc6a14d9dd03b94b35c521f3
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-08-01 22:54:50 +0530

    Time: 1 ms (96.17%) | Memory: 43.9 MB (44.03%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max = candies[0];
+
+        int n = candies.length;
+
+        for(int i=1;i<n;i++){
+
+            if(candies[i]>max){
+
+                max = candies[i];
+            }
+        }
+
+        List<Boolean> res = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+
+            if(candies[i]+extraCandies>=max){
+
+                res.add(true);
+            }
+            else{
+
+                res.add(false);
+            }
+        }
+
+        return res;
+        
+    }
+}
