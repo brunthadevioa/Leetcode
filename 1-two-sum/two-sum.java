9406146f1f4632d4c6e1c289d723721b69663bed
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-08-09 20:47:53 +0530

    Time: 39 ms (37.56%) | Memory: 47.1 MB (59.54%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,19 +3,23 @@ class Solution {
 
         int n = nums.length;
 
-        
-        
+        int ans[] = new int[2];
+
         for(int i=0;i<n;i++){
 
             for(int j=i+1;j<n;j++){
-                
+
                 if(nums[i]+nums[j]==target){
 
-                    return new int[]{i,j};
+                    ans[0]=i;
+
+                    ans[1]=j;
+
+                    return ans;
                 }
             }
-       }
-
-       return new int[]{};
+        }
+        
+        return ans;
     }
 }
