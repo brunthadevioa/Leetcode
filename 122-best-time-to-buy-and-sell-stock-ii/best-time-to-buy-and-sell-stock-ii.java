c9d70285d5a67d2e931c947e6de60e604b471f88
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-08-09 21:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (93.53%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
+
         int n = prices.length;
 
         int p = 0;
 
         for(int i=0;i<n-1;i++){
 
-            if(prices[i]<prices[i+1]){
+            if(prices[i]<prices[i+1])
 
                 p+=prices[i+1] - prices[i];
-            }
+            
         }
 
         return p;
+        
     }
 }
