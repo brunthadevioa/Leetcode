5f993f8f216ba443735dfd04dbd1626e6bde7394
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-08-10 10:11:57 +0530

    Time: 63 ms (50.61%) | Memory: 107.9 MB (7.24%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,25 +1,29 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
+        
 
         int n = temp.length;
 
         Stack<Integer> stk = new Stack<>();
 
-        int res[] = new int[n];
+        int [] ans = new int[n];
 
         for(int i=0;i<n;i++){
 
             while(!stk.isEmpty() && temp[i]>temp[stk.peek()]){
 
-                int in = stk.pop();
+             int in = stk.pop();
 
-                res[in] = i-in;
+                ans[in] = i - in;
             }
 
             stk.push(i);
         }
 
-        return res;
+        return ans;
 
+        
     }
+
+    
 }
