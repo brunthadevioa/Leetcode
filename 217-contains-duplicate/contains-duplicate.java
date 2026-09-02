091a5d0a9533a3243a4d554827c5c6f6110251fd
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-09-02 20:52:00 +0530

    Time: 13 ms (98.93%) | Memory: 108.9 MB (15.35%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n : nums){
+
+            if(!set.add(n))
+
+            return true;
+        }
+
+        return false;
+        
+    }
+}
