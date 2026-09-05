d0953f07f60bb7fcdf7fb9f1dc2b1fca239902c0
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-09-05 16:20:04 +0530

    Time: 1 ms (99.79%) | Memory: 46.6 MB (15.79%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+        int max = Integer.MIN_VALUE;
+
+        int min = Integer.MAX_VALUE;
+
+        int n = nums.length;
+
+        int[] p = new int[n];
+
+        int[] s = new int[n];
+
+        for(int i=0;i<n;i++){
+
+            max = Math.max(max,nums[i]);
+
+            p[i] = max;
+        }
+
+        for(int i=n-1;i>=0;i--){
+
+            min = Math.min(min,nums[i]);
+
+            s[i] = min;
+        }
+
+        for(int i=0;i<n;i++){
+
+            if(p[i] - s[i]<=k) return i;
+        }
+
+        return -1;
+        
+           
+    }
+}
